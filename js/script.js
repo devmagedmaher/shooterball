d71124026f8c13aa2aac0d6ff11e2d206b81f7cc
= <=>
2019-05-21 03:23:54 +0200

    randomize enemy start location out of canvas

--- js/script.js
+++ js/script.js
@@ -1,40 +1,38 @@
 
 // top canvas
-var canvasTop = document.getElementById('data');
-var ct = canvasTop.getContext('2d');
-canvasTop.width = window.innerWidth;
+  var canvasTop = document.getElementById('data');
+  var ct = canvasTop.getContext('2d');
+  canvasTop.width = window.innerWidth;
 
 // canvas area
-var canvas = document.getElementById('canvas');
-var c = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight - canvasTop.height;
+  var canvas = document.getElementById('canvas');
+  var c = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight - canvasTop.height;
 
-var data = {
-  playing: false,
-  
-  bullets: 0,
-  enemies: 0,
-  sparks: 0
-}
+// text data
+  var data = {
+    playing: false,
+    
+    bullets: 0,
+    enemies: 0,
+    sparks: 0
+  }
 
-
-function drawText() {
-  ct.clearRect(0, 0, canvasTop.width, canvasTop.height);
-  ct.beginPath();
-  ct.fillStyle = '#dadada';
-  ct.font = '16px tahoma';
-	ct.textBaseline = 'middle';
-  ct.textAlign = 'left';
-  ct.fillText(`Bullets: ${data.bullets}`, 20, canvasTop.height/2);
-  ct.fillText(`Enemies: ${data.enemies}`, 130, canvasTop.height/2);
-  ct.fillText(`Sparks: ${data.sparks}`, 240, canvasTop.height/2);
-  ct.textAlign = 'right';
-  ct.fillText('righ text here...', canvas.width - 40, canvasTop.height/2);
-}
-drawText();
-
-
+  function drawText() {
+    ct.clearRect(0, 0, canvasTop.width, canvasTop.height);
+    ct.beginPath();
+    ct.fillStyle = '#dadada';
+    ct.font = '16px tahoma';
+  	ct.textBaseline = 'middle';
+    ct.textAlign = 'left';
+    ct.fillText(`Bullets: ${data.bullets}`, 20, canvasTop.height/2);
+    ct.fillText(`Enemies: ${data.enemies}`, 130, canvasTop.height/2);
+    ct.fillText(`Sparks: ${data.sparks}`, 240, canvasTop.height/2);
+    ct.textAlign = 'right';
+    ct.fillText('Click to START game..     press Esc to EXIT ', canvas.width - 40, canvasTop.height/2);
+  }
+  drawText();
 
 
 
@@ -45,37 +43,47 @@ drawText();
 
 
 
-var game = {
-  bulletInterval: 60 * 0.01,
-  bulletSizeMin: 1,
-  bulletSizeMax: 3,
-  
-  enemyInterval: 60 * 0.09,
-  enemySpeed: 0.2,
-  enemySizeMin: 10,
-  enemySizeMax: 20,
-  
-  sparksMin: 10,
-  sparksMax: 30,
-  sparkSizeMin: 0.5,
-  sparkSizeMax: 2,
-  
-};
-var mouse = {
-  xSpeed: 0,
-  ySpeed: 0,
-  down: false
-}
-var key = {
-  left: false,
-  right: false,
-  up: false,
-  down: false,
-};
 
-var bullets = [];
-var enemies = [];
-var sparks = [];
+// game variables
+  var game = {
+    bulletInterval: 60 * 0.01,
+    bulletSizeMin: 1,
+    bulletSizeMax: 3,
+    
+    enemyInterval: 60 * 0.09,
+    enemySpeed: 0.2,
+    enemySizeMin: 10,
+    enemySizeMax: 20,
+    
+    sparksMin: 10,
+    sparksMax: 30,
+    sparkSizeMin: 0.5,
+    sparkSizeMax: 2,
+
+    play: function () {
+      game.playing = true;
+    },
+
+    pause: function () {
+      game.playing = false;
+    }
+    
+  };
+  var mouse = {
+    xSpeed: 0,
+    ySpeed: 0,
+    down: false
+  }
+  var key = {
+    left: false,
+    right: false,
+    up: false,
+    down: false,
+  };
+
+  var bullets = [];
+  var enemies = [];
+  var sparks = [];
 
 
 
@@ -290,6 +298,7 @@ function Spark(x, y, color = '#fff') {
   this.delete = function () {
     sparks.splice(sparks.indexOf(this), 1);
   };
+
 }
 
 
@@ -385,8 +394,21 @@ function Bullet() {
 
 
 function Enemy() {
-  this.x = 0;
-  this.y = 0;
+  this.i = Math.floor(Math.random() * 4);
+  this.xArray = [
+    Math.random() * canvas.width,
+    Math.random() * canvas.width,
+    -100,
+    canvas.width + 100
+  ];
+  this.yArray = [
+    -100,
+    canvas.height + 100,
+    Math.random() * canvas.height,
+    Math.random() * canvas.height
+  ];
+  this.x = this.xArray[this.i];
+  this.y = this.yArray[this.i];
   this.speed = Math.random() * (game.enemySpeed-(game.enemySpeed/2) + (game.enemySpeed/2))
   this.xSpeed = 0;
   this.ySpeed = 0;
@@ -461,6 +483,7 @@ function Enemy() {
   this.delete = function () {
     enemies.splice(enemies.indexOf(this), 1);
   }
+
 }
 
 
@@ -477,166 +500,160 @@ function Enemy() {
 
 
 // counters
-var bulletIntervalCounter = 0;
-var enemyIntervalCounter = 0;
+  var bulletIntervalCounter = 0;
+  var enemyIntervalCounter = 0;
 
-function draw() {
-  if (game.playing) {
 
-    c.clearRect(0, 0, canvas.width, canvas.height);
 
-    // draw player
-    player.render();
 
-    // draw target
-    target.render();
+// draw loop function
+  function draw() {
+    if (game.playing) {
 
-    // add bullets to array
-    if (mouse.down) {
-      bulletIntervalCounter++;
-      if (bulletIntervalCounter >= game.bulletInterval) {
-        bullets.push(new Bullet());
+      c.clearRect(0, 0, canvas.width, canvas.height);
+
+      // draw player
+      player.render();
+
+      // draw target
+      target.render();
+
+      // add bullets to array
+      if (mouse.down) {
+        bulletIntervalCounter++;
+        if (bulletIntervalCounter >= game.bulletInterval) {
+          bullets.push(new Bullet());
+          bulletIntervalCounter = 0;
+        }
+      } else {
         bulletIntervalCounter = 0;
       }
+
+      // draw bullets
+      for (bullet of bullets) {
+        bullet.render();
+      }
+
+      // add enemies to array
+      enemyIntervalCounter++;
+      if (enemyIntervalCounter >= game.enemyInterval) {
+        enemyIntervalCounter = 0;
+        enemies.push(new Enemy());
+      }
+
+      // draw enemies
+      for (enemy of enemies) {
+        enemy.render();
+      }
+
+
+      // draw sparks
+      for (spark of sparks) {
+        spark.render();
+      }
+
+      // draw text
+      data.bullets = bullets.length;
+      data.enemies = enemies.length;
+      data.sparks = sparks.length;
+      drawText();
+
+    }
+    requestAnimationFrame(draw);  
+  }
+  draw();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Controls Handling
+
+  canvas.requestPointerLock = canvas.requestPointerLock || canvas.mozRequestPointerLock;
+
+  document.exitPointerLock = document.exitPointerLock || document.mozExitPointerLock;
+
+  canvas.onclick = function() {
+    canvas.requestPointerLock();
+  }
+
+  document.addEventListener('pointerlockchange', lockChangeAlert, false);
+  document.addEventListener('mozpointerlockchange', lockChangeAlert, false);
+
+
+  function lockChangeAlert() {
+    if (document.pointerLockElement === canvas ||
+        document.mozPointerLockElement === canvas) {
+      console.log('The pointer lock status is now locked');
+      document.addEventListener("mousemove", updatePosition, false);
+
+      document.addEventListener('mousedown', mouseDownHandler, false);
+      document.addEventListener('mouseup', mouseUpHandler, false);
+      
+      document.addEventListener('keydown', keydownHandler, false);
+      document.addEventListener('keyup', keyupHandler, false);
+      
+      game.play();
     } else {
-      bulletIntervalCounter = 0;
+      console.log('The pointer lock status is now unlocked');
+      document.removeEventListener("mousemove", updatePosition, false);
+
+      document.removeEventListener('mousedown', mouseDownHandler, false);
+      document.removeEventListener('mouseup', mouseUpHandler, false);
+      
+      document.removeEventListener('keydown', keydownHandler, false);
+      document.removeEventListener('keyup', keyupHandler, false);
+
+      game.pause();
     }
-
-    // draw bullets
-    for (bullet of bullets) {
-      bullet.render();
-    }
-
-    // add enemies to array
-    enemyIntervalCounter++;
-    if (enemyIntervalCounter >= game.enemyInterval) {
-      enemyIntervalCounter = 0;
-      enemies.push(new Enemy());
-    }
-
-    // draw enemies
-    for (enemy of enemies) {
-      enemy.render();
-    }
-
-
-    // draw sparks
-    for (spark of sparks) {
-      spark.render();
-    }
-
-    // draw text
-    data.bullets = bullets.length;
-    data.enemies = enemies.length;
-    data.sparks = sparks.length;
-    drawText();
-
   }
-  requestAnimationFrame(draw);  
-}
-draw();
 
-
-
-
-
-
-
-function play() {
-  game.playing = true;
-}
-
-function pause() {
-  game.playing = false;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// pointer lock
-
-canvas.requestPointerLock = canvas.requestPointerLock ||
-                            canvas.mozRequestPointerLock;
-
-document.exitPointerLock = document.exitPointerLock ||
-                           document.mozExitPointerLock;
-
-canvas.onclick = function() {
-  canvas.requestPointerLock();
-}
-
-document.addEventListener('pointerlockchange', lockChangeAlert, false);
-document.addEventListener('mozpointerlockchange', lockChangeAlert, false);
-
-
-function lockChangeAlert() {
-  if (document.pointerLockElement === canvas ||
-      document.mozPointerLockElement === canvas) {
-    console.log('The pointer lock status is now locked');
-    document.addEventListener("mousemove", updatePosition, false);
-
-    document.addEventListener('mousedown', mouseDownHandler, false);
-    document.addEventListener('mouseup', mouseUpHandler, false);
-    
-    document.addEventListener('keydown', keydownHandler, false);
-    document.addEventListener('keyup', keyupHandler, false);
-    
-    play();
-  } else {
-    console.log('The pointer lock status is now unlocked');
-    document.removeEventListener("mousemove", updatePosition, false);
-
-    document.removeEventListener('mousedown', mouseDownHandler, false);
-    document.removeEventListener('mouseup', mouseUpHandler, false);
-    
-    document.removeEventListener('keydown', keydownHandler, false);
-    document.removeEventListener('keyup', keyupHandler, false);
-
-    pause();
+  function updatePosition(e) {
+    mouse.xSpeed += e.movementX;
+    mouse.ySpeed += e.movementY;
   }
-}
 
-function updatePosition(e) {
-  mouse.xSpeed += e.movementX;
-  mouse.ySpeed += e.movementY;
-}
+  function mouseDownHandler() {
+    mouse.down = true;
+  }
 
-function mouseDownHandler() {
-  mouse.down = true;
-}
-
-function mouseUpHandler() {
-  mouse.down = false;
-}
+  function mouseUpHandler() {
+    mouse.down = false;
+  }
 
 
-function keydownHandler(e) {
-  if (e.key == 'w' || e.key == 'Up' || e.key == 'ArrowUp') { key.up = true; }
-  if (e.key == 'a' || e.key == 'Left' || e.key == 'ArrowLeft') { key.left = true; }
-  if (e.key == 's' || e.key == 'Down' || e.key == 'ArrowDown') { key.down = true; }
-  if (e.key == 'd' || e.key == 'Right' || e.key == 'ArrowRight') { key.right = true; }
-}
+  function keydownHandler(e) {
+    if (e.key == 'w' || e.key == 'Up' || e.key == 'ArrowUp') { key.up = true; }
+    if (e.key == 'a' || e.key == 'Left' || e.key == 'ArrowLeft') { key.left = true; }
+    if (e.key == 's' || e.key == 'Down' || e.key == 'ArrowDown') { key.down = true; }
+    if (e.key == 'd' || e.key == 'Right' || e.key == 'ArrowRight') { key.right = true; }
+  }
 
-function keyupHandler(e) {
-  if (e.key == 'w' || e.key == 'Up' || e.key == 'ArrowUp') { key.up = false; }
-  if (e.key == 'a' || e.key == 'Left' || e.key == 'ArrowLeft') { key.left = false; }
-  if (e.key == 's' || e.key == 'Down' || e.key == 'ArrowDown') { key.down = false; }
-  if (e.key == 'd' || e.key == 'Right' || e.key == 'ArrowRight') { key.right = false; }
-}
+  function keyupHandler(e) {
+    if (e.key == 'w' || e.key == 'Up' || e.key == 'ArrowUp') { key.up = false; }
+    if (e.key == 'a' || e.key == 'Left' || e.key == 'ArrowLeft') { key.left = false; }
+    if (e.key == 's' || e.key == 'Down' || e.key == 'ArrowDown') { key.down = false; }
+    if (e.key == 'd' || e.key == 'Right' || e.key == 'ArrowRight') { key.right = false; }
+  }
 
 
 
